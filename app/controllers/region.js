c64f9794181c1809e1eb3d7b198730a1891746ec
Gad AMAR <amar@MacBook-Pro-de-Gad.local>
2015-12-29 22:16:12 +0100

    Initial commit dated 29/12/2015

---
+++ app/controllers/region.js
@@ -0,0 +1,36 @@
+/**
+ * Module dependencies.
+ */
+
+var mongoose = require('mongoose');
+var Article = mongoose.model('Article');
+
+/**
+ * List items tagged with a tag
+ */
+
+exports.index = function (req, res) {
+  var criteria = { region: req.params.region };
+  var perPage = 5;
+  var page = (req.params.page > 0 ? req.params.page : 1) - 1;
+  console.log("--page = ", page, "-- criteria = ", criteria);
+  var options = {
+    perPage: perPage,
+    page: page,
+    criteria: criteria
+  };
+
+  Article.list(options, function(err, articles) {
+    if (err) return res.render('500');
+    Article.count(criteria).exec(function (err, count) {
+        console.log("nb article", count);
+      res.render('articles/index', {
+        icon: 'fa fa-map-marker',
+        title: 'Bons plans pour la région ' + req.params.region ,
+        articles: articles,
+        page: page + 1,
+        pages: Math.ceil(count / perPage)
+      });
+    });
+  });
+};
